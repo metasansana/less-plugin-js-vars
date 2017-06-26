a17fa8bd3e6d8344eb2f47e321721773e2c5ce19
Lasana Murray <lmurray@quenk.com>
2017-06-26 19:38:50 -0400

    chore: first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+Less Plugin JS Vars
+===================
+
+A less css plugin that let's use js vars all over the place.
+
+
+## Installation
+
+```
+npm install --save-dev less-plugin-js-vars
+
+```
+
+## Usage
+```
+lessc file.less --js-vars="vars.js"
+
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var polate = require('@quenk/polate').polate;
+
+function VariablePreProcessor(context) {
+
+    this.context = context || {};
+
+}
+
+VariablePreProcessor.prototype = {
+
+    process: function(src, data) {
+
+        return polate(src, this.context, { start: '{{', end: '}}' });
+
+    }
+
+};
+
+function JSVarsPlugin(filename) {
+
+    this.minVersion = [2, 0, 0];
+    this.options = { filename: filename || 'less.js' };
+
+}
+
+JSVarsPlugin.prototype = {
+
+    install: function(less, pluginManager) {
+
+        pluginManager.addPreProcessor(new VariablePreProcessor(
+            require(require.resolve(path.join(process.cwd(), this.options.filename)))));
+
+    },
+
+    setOptions: function(filename) {
+
+        this.options = { filename: filename };
+
+    },
+
+    printUsage: function() {
+
+        return '--js-vars=options';
+
+    }
+
+};
+
+module.exports = JSVarsPlugin;
